311c5729b8c95c191ec1a06571a2f800d11820e6
Euregan <va.mcxp@gmail.com>
2020-04-13 12:39:48 +0200

    forced websocket port

--- pages/_app.js
+++ pages/_app.js
@@ -13,7 +13,7 @@ const App = ({ Component }) => {
 
     useEffect(() => {
         if (typeof window !== 'undefined') {
-            window.pool = new Pool(`${window.location.protocol === 'http:' ? 'ws' : 'wss'}://${window.location.host}`)
+            window.pool = new Pool(`${window.location.protocol === 'http:' ? 'ws' : 'wss'}://${window.location.hostname}:3000`)
             setPool(pool);
         }
     }, []);
