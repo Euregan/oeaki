182a2a5690339506093555d8c68d51d02483ea23
Euregan <va.mcxp@gmail.com>
2020-04-14 19:10:04 +0200

    Pushing the websocket to Heroku since Zeit can't handle it

---
+++ signaling.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const WebSocket = require('ws');
+
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer();
+
+server.listen(port, (err) => {
+    if (err) throw err;
+    // eslint-disable-next-line no-console
+    console.log(`> Ready on http://localhost:${port}`);
+});
+
+const wsServer = new WebSocket.Server({ server }).on('connection', (ws) => {
+    ws.on('message', (message) => {
+        wsServer.clients.forEach((client) => {
+            if (client !== ws) {
+                client.send(message);
+            }
+        });
+    });
+});
