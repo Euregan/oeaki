ec9720ec1d2a6c41bde22ca6e507f4e821cb1eff
Euregan <va.mcxp@gmail.com>
2020-04-15 17:56:12 +0200

    moved id management on the server side and implemented a room system #15 #20

--- signaling.js
+++ signaling.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const WebSocket = require('ws');
+const { v4 } = require('uuid');
 
 const PORT = process.env.PORT || 3001;
 
@@ -11,12 +12,40 @@ server.listen(PORT, (err) => {
     console.log(`> Ready on http://localhost:${PORT}`);
 });
 
-const wsServer = new WebSocket.Server({ server }).on('connection', (ws) => {
-    ws.on('message', (message) => {
-        wsServer.clients.forEach((client) => {
-            if (client !== ws) {
-                client.send(message);
+let rooms = {};
+
+const wsServer = new WebSocket.Server({ server }).on('connection', (ws, request) => {
+    const id = v4();
+    const room = request.url;
+
+    // Adding the client to the room
+    if (!rooms[room]) {
+        rooms[room] = {};
+    }
+    rooms[room][id] = ws;
+
+    // Transmitting the messages
+    ws.on('message', (raw) => {
+        // Parsing the message and adding the sender
+        const message = {
+            ...JSON.parse(raw),
+            sender: id,
+        };
+        // For each client in the room
+        Object.keys(rooms[room]).forEach((peerId) => {
+            // If there is no particular recipient, or if the recipient is the current client
+            if (peerId !== id && (!message.recipient || message.recipient === peerId)) {
+                // Forward the message
+                rooms[room][peerId].send(JSON.stringify(message));
             }
         });
+    });
+
+    // Cleaning up after a client has left
+    ws.on('close', () => {
+        delete rooms[room][id];
+        if (Object.keys(rooms[room]).length === 0) {
+            delete rooms[room];
+        }
     });
 });
