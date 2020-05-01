68a410f8f3cd28d0a388742c3bf1315642f593ab
zyhou <richard.maxime9@gmail.com>
2020-05-01 15:46:08 +0200

    remove useless import

--- components/Canvas.js
+++ components/Canvas.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes, { func } from 'prop-types';
+import PropTypes from 'prop-types';
 import useWindowSize from '../lib/useWindowSize';
 import { Card } from './UI';
 
